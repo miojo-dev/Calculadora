deac73d56e3b05fe25dbafa5502e4dd25e95f48b
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2024-08-12 16:23:33 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,41 @@
+#Calculadora em função única
+
+def calculadora():
+    operacao = input("Escolha uma operação (+, -, *, /): ")
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if operacao == "+":
+        resultado = adicao(num1, num2)
+        print("Resultado: ", resultado)
+        
+    elif operacao == "-":
+        resultado = subtracao(num1, num2)
+        print("Resultado: ", resultado)
+        
+    elif operacao == "*":
+        resultado = multiplicacao(num1, num2)
+        print("Resultado: ", resultado)
+        
+    elif operacao == "/":
+        resultado = divisao(num1, num2)
+        print("Resultado: ", resultado)
+
+    else: 
+        print("Operação inválida!")
+
+calculadora()
+
+#Calculadora com funções separadas por operadores
+
+def adicao(a, b)
+    return a + b
+
+def subtracao(a, b)
+    return a - b
+
+def multiplicacao(a, b)
+    return a * b
+
+def divisao(a, b)
+    return a / b
